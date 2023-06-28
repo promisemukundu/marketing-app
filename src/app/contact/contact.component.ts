c487b0b119bd1a8a153121f1bb4be8e7916ac9c1
Promise Mukundu <innocent.mazando@falcorp.co.za>
2023-06-28 13:38:42 +0200

    added all forms

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CompanyDetails } from '../shared/models/company-details';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  submitForm: FormGroup;
 
+  constructor(private http: HttpClient) {
+    this.submitForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      subject: new FormControl(null, Validators.required),
+      message: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.submitForm.value);
+    this.submitForm.reset();
+  }
+
+  details?: CompanyDetails;
+  isLoading: boolean = true;
+
+  getDetails() {
+    this.http
+      .get<CompanyDetails>('http://localhost:1337/api/company-detail')
+      .subscribe((response) => {
+        console.log(response);
+        this.details = response;
+        console.log(this.details);
+      });
+  }
+
+  ngOnInit(): void {
+    this.getDetails();
+  }
 }
--- src/app/home/qoute/qoute.component.ts
+++ src/app/home/qoute/qoute.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CompanyDetails } from 'src/app/shared/models/company-details';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-qoute',
@@ -8,20 +10,42 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class QouteComponent implements OnInit {
   submitForm: FormGroup;
+  details?: CompanyDetails;
+  isLoading: boolean = true;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.submitForm = new FormGroup({
       name: new FormControl(null, Validators.required),
       email: new FormControl(null, [Validators.required, Validators.email]),
       number: new FormControl(null, Validators.required),
       message: new FormControl(null, Validators.required),
+      service: new FormControl(null, Validators.required),
     });
   }
 
+
   onSubmit() {
-    console.log(this.submitForm);
-    //this.submitForm.reset()
+    console.log(this.submitForm.value);
+    this.submitForm.reset();
   }
 
-  ngOnInit(): void { }
+
+
+  getDetails() {
+    this.http
+      .get<CompanyDetails>('http://localhost:1337/api/company-detail')
+      .subscribe((response) => {
+        console.log(response);
+        this.details = response;
+        console.log(this.details);
+      });
+  }
+
+
+
+
+  ngOnInit(): void {
+    this.getDetails();
+  }
+
 }
